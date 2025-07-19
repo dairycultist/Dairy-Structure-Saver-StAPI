84d1b7b061dd4bc1c3d8bcf2465e0f89a8bd7b0e
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-07-18 20:50:01 -0400

    Structure paste working

--- src/main/java/net/dairycultist/dss/DairyStructureSaver.java
+++ src/main/java/net/dairycultist/dss/DairyStructureSaver.java
@@ -22,9 +22,19 @@ public class DairyStructureSaver implements ModInitializer {
 
                 public void command(SharedCommandSource sharedCommandSource, String[] strings) {
 
+                    if (strings.length == 1) {
+                        sharedCommandSource.sendFeedback("Syntax: /structure [corner|paste|save]");
+                        return;
+                    }
+
                     switch (strings[1].toLowerCase()) {
 
                         case "corner":
+
+                            if (strings.length != 3) {
+                                sharedCommandSource.sendFeedback("Syntax: /structure corner [1|2]");
+                                return;
+                            }
 
                             switch (strings[2]) {
 
@@ -71,18 +81,49 @@ public class DairyStructureSaver implements ModInitializer {
                         // /structure paste ~ ~ ~
                         case "paste":
 
-                            for (int x = x1; x < x2; x++) {
-                                for (int y = y1; y < y2; y++) {
-                                    for (int z = z1; z < z2; z++) {
-                                        sharedCommandSource.getPlayer().world.setBlock(x, y, z, Block.DIAMOND_BLOCK.id);
+                            if (
+                                    strings.length != 5
+                                    || isNotValidBlockPos(strings[2])
+                                    || isNotValidBlockPos(strings[3])
+                                    || isNotValidBlockPos(strings[4])
+                            ) {
+                                sharedCommandSource.sendFeedback("Syntax: /structure paste x y z (either int or ~)");
+                                return;
+                            }
+
+                            int posX = getBlockPos(strings[2], (int) sharedCommandSource.getPlayer().x);
+                            int posY = getBlockPos(strings[3], (int) sharedCommandSource.getPlayer().y);
+                            int posZ = getBlockPos(strings[4], (int) sharedCommandSource.getPlayer().z);
+
+                            // check to make sure there isn't overlap
+                            if (
+                                    Math.abs(posX - x1) < x2 - x1 &&
+                                    Math.abs(posY - y1) < y2 - y1 &&
+                                    Math.abs(posZ - z1) < z2 - z1
+                            ) {
+                                sharedCommandSource.sendFeedback("Paste structure cannot overlap source structure!");
+                                return;
+                            }
+
+                            for (int x = 0; x < x2 - x1; x++) {
+                                for (int y = 0; y < y2 - y1; y++) {
+                                    for (int z = 0; z < z2 - z1; z++) {
+                                        sharedCommandSource.getPlayer().world.setBlock(posX + x, posY + y, posZ + z, sharedCommandSource.getPlayer().world.getBlockId(x1 + x, y1 + y, z1 + z));
                                     }
                                 }
                             }
-
                             break;
 
                         // /structure save house
                         case "save":
+
+                            for (int y = y1; y < y2; y++) {
+                                for (int x = x1; x < x2; x++) {
+                                    for (int z = z1; z < z2; z++) {
+                                        System.out.println(sharedCommandSource.getPlayer().world.getBlockId(x, y, z));
+                                    }
+                                }
+                            }
                             break;
 
                         default:
@@ -106,5 +147,26 @@ public class DairyStructureSaver implements ModInitializer {
             System.err.println("\n\n###\nDairyStructureSaver commands could not be initialized!\nPlease check you have retrocommands installed!\n###\n");
         }
 
+    }
+
+    private static boolean isNotValidBlockPos(String str) {
+
+        if (str.equals("~"))
+            return false;
+
+        try {
+            Integer.parseInt(str);
+            return false;
+        } catch (NumberFormatException e) {
+            return true;
+        }
+    }
+
+    private static int getBlockPos(String str, int local) {
+
+        if (str.equals("~"))
+            return local;
+
+        return Integer.parseInt(str);
     }
 }
